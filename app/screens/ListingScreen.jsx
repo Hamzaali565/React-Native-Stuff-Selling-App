d51ddf47949f3764b1a7e7d10e8dc4cf24fcce98
stallyonsDev <98030644+stallyonsDev@users.noreply.github.com>
2023-01-26 19:52:28 +0500

    .

--- app/screens/ListingScreen.jsx
+++ app/screens/ListingScreen.jsx
@@ -6,7 +6,7 @@ import colors from "../config/colors";
 
 const Listings = [
   {
-    id: 2,
+    id: 1,
     title: "Red jacket for sale",
     price: 100,
     image: require("../assets/jacket.jpg"),
